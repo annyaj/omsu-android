94c47e4c99e093918b6dc1252a51b398d147252c
anna_kiseleva <anna_kiseleva@lineate.com>
2020-09-19 16:48:37 +0600

    added two activities for 6 day

---
+++ candyshop/src/main/java/com/example/candyshop/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.candyshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton unicornButton = findViewById(R.id.unicornButton);
+        unicornButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
