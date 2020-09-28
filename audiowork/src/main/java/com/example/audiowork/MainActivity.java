02d3288571bc812481af9256442e34b4395be440
anna_kiseleva <anna_kiseleva@lineate.com>
2020-09-28 20:28:12 +0600

    Added day 16 - work with audio

---
+++ audiowork/src/main/java/com/example/audiowork/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.audiowork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SoundPool mSoundPool;
+    private AssetManager mAssetManager;
+    private int alarmSound, rainSound, toastsSound, toasterSound;
+    private int mStreamID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button alarmButton = findViewById(R.id.alarm_button);
+        alarmButton.setOnClickListener(onClickListener);
+
+        Button rainImageButton = findViewById(R.id.rain_button);
+        rainImageButton.setOnClickListener(onClickListener);
+
+        Button toasterButton = findViewById(R.id.toaster_button);
+        toasterButton.setOnClickListener(onClickListener);
+
+        Button toastsButton = findViewById(R.id.toasts_button);
+        toastsButton.setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.alarm_button:
+                    playSound(alarmSound);
+                    break;
+                case R.id.rain_button:
+                    playSound(rainSound);
+                    break;
+                case R.id.toaster_button:
+                    playSound(toasterSound);
+                    break;
+                case R.id.toasts_button:
+                    playSound(toastsSound);
+                    break;
+            }
+        }
+    };
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void createNewSoundPool() {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        mSoundPool = new SoundPool.Builder()
+                .setAudioAttributes(attributes)
+                .build();
+    }
+
+    @SuppressWarnings("deprecation")
+    private void createOldSoundPool() {
+        mSoundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    private int playSound(int sound) {
+        if (sound > 0) {
+            mStreamID = mSoundPool.play(sound, 1, 1, 1, 0, 1);
+        }
+        return mStreamID;
+    }
+
+    private int loadSound(String fileName) {
+        AssetFileDescriptor afd;
+        try {
+            afd = mAssetManager.openFd(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Не могу загрузить файл " + fileName,
+                    Toast.LENGTH_SHORT).show();
+            return -1;
+        }
+        return mSoundPool.load(afd, 1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            createOldSoundPool();
+        } else {
+            createNewSoundPool();
+        }
+
+        mAssetManager = getAssets();
+
+        alarmSound = loadSound("budilnik.ogg");
+        rainSound = loadSound("rain.ogg");
+        toasterSound = loadSound("toster.ogg");
+        toastsSound = loadSound("tosts.ogg");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSoundPool.release();
+        mSoundPool = null;
+    }
+}
