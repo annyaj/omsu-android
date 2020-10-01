d9c1b563be37505f19cab658b69b219923858d4a
anna_kiseleva <anna_kiseleva@lineate.com>
2020-10-01 21:32:59 +0600

    added my browser

---
+++ mybrowser/src/main/java/com/example/mybrowser/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mybrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/"));
+        startActivity(intent);
+    }
+
+}
